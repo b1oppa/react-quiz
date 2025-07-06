e5219b02e6e0026aa1ecf77fb85b95970b4d3603
Sarthak Saraswat <sarthaksaraswat298@gmail.com>
2025-07-06 21:29:25 +0530

    added restart functionality

--- src/components/App.js
+++ src/components/App.js
@@ -64,6 +64,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
 
     default:
       throw new Error("Action unknown");
@@ -132,6 +134,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
